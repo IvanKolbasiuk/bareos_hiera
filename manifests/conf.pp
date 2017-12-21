b5b7b4ae33313628bd72e43a867c2845543570c0
IvanKolbasiuk <kolbasyuk@gmail.com>
2017-12-21 10:41:54 +0200

    Update conf.pp

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -1,4 +1,5 @@
 class bareos::conf {
+require bareos
 file {'/etc/bareos/bareos-dir.d/catalog/MyCatalog.conf':
     ensure => file,
     content => 'catalog {
@@ -8,6 +9,7 @@ dbname = bareos
 dbdriver = mysql
 dbpassword = bareos
 }',
+notify => Service['bareos-sd', 'bareos-fd', 'bareos-dir'],
 }
 file {'/etc/bareos/bareos-dir.d/schedule/TenMinutes.conf':
     ensure => file,
@@ -20,6 +22,7 @@ file {'/etc/bareos/bareos-dir.d/schedule/TenMinutes.conf':
   Run = Level=Full hourly at 0:45
   Run = Level=Full hourly at 0:55
 }',
+notify => Service['bareos-sd', 'bareos-fd', 'bareos-dir'],
 }
 file {'/etc/bareos/bareos-dir.d/job/myapp.conf':
     ensure => file,
@@ -32,6 +35,7 @@ file {'/etc/bareos/bareos-dir.d/job/myapp.conf':
   FileSet="Myapp"
   Schedule = "TenMinutes"
 }',
+notify => Service['bareos-sd', 'bareos-fd', 'bareos-dir'],
 }
 file {'/etc/bareos/bareos-dir.d/fileset/Myapp.conf':
     ensure => file,
@@ -44,6 +48,7 @@ file {'/etc/bareos/bareos-dir.d/fileset/Myapp.conf':
      File = /var/lib/mysql
     }
 }',
+notify => Service['bareos-sd', 'bareos-fd', 'bareos-dir'],
 }
 file {'/etc/bareos/bareos-sd.d/device/FileStorage.conf':
      ensure => file,
@@ -59,8 +64,6 @@ file {'/etc/bareos/bareos-sd.d/device/FileStorage.conf':
   AlwaysOpen = yes;
   Description = "File device. A connecting Director must have the same Name and MediaType."
 }',
+notify => Service['bareos-sd', 'bareos-fd', 'bareos-dir'],
 }
-notify => Service['bareos-sd'],
-notify => Service['bareos-fd'],
-notify => Service['bareos-dir'],
 }
