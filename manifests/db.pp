0c95e92f431fcfdaa666de5f8de9842794dda24e
IvanKolbasiuk <kolbasyuk@gmail.com>
2017-12-21 14:58:20 +0200

    Update db.pp

--- manifests/db.pp
+++ manifests/db.pp
@@ -7,18 +7,18 @@ $db_type = 'mysql'
 exec { "create-db":
       unless => "/usr/bin/mysql -uroot bareos",
       command => "/usr/bin/mysql -uroot -e \"create database bareos;\"",
-      require => Package['mysql-server'],
+      require => Service['mysql'],
       }
  exec { "grant--db":
       unless => "/usr/bin/mysql -ubareos -pbareos bareos",
       command => "/usr/bin/mysql -uroot -e \"grant all on bareos.* to 'bareos'@'localhost' identified by 'bareos';\"",
-      require => Package['mysql-server'],
+      require => Service['mysql'],
      }
  
 
 exec { 'make_tables':
          command => "${script_directory}/make_bareos_tables ${db_type} ${db_parameters}",
-           refreshonly  => true, 
+            
       }
 exec { 'grant_privileges':
          command  => "${script_directory}/grant_bareos_privileges ${db_type} ${db_parameters}",
